ba14034e27af85387a368ffff94b44aca0b7ad40
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2020-12-09 20:30:22 +0600

    shop page added with collection preview and item

---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+import SHOP_DATA from "./ShopData";
+import Collection from "../../components/collection-preview/CollectionPreview";
+
+export class Shop extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <Collection key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Shop;
